a976223763c57b1edc71a5cc0e249488fa560dde
alvesph <alvesph1@gmail.com>
2020-07-06 00:01:17 -0400

    UPDATE CHALENGE

--- style.js
+++ style.js
@@ -1,5 +1,11 @@
 var boxElement = document.querySelector('.desafio');
 
+boxElement.style.position = 'relative'
+boxElement.style.border = 'outset 2px #4CAF50';
 boxElement.style.width = 100;
 boxElement.style.height = 100;
-boxElement.style.backgroundColor = 'green';
+boxElement.style.backgroundColor = '#cae8ca';
+
+var textoElement = document.querySelector("a");
+
+textoElement.style.color = 'green';
